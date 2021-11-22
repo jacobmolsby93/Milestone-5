a9b119d53a457df4bb9aa6347ba7d7d93b579f10
Jacob Molsby <jacobmolsby@hotmail.se>
2021-11-22 05:19:58 +0000

    Add services and templates

--- services/views.py
+++ services/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Services
 
 # Create your views here.
+
+def services(request):
+    """
+    A view to show the services which the site provides.
+    """
+    services = Services.objects.all()
+
+    context = {
+        'services': services,
+    }
+    
+    return render(request, 'services/services.html', context)
