ef55d5c768a97d26d612b4f0b43f91726d46d767
Jacob Molsby <jacobmolsby@hotmail.se>
2021-11-23 11:39:18 +0000

    Added search query to styles

--- styles/views.py
+++ styles/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib import messages
+from django.db.models import Q
 from .models import ShopStyles
+from services.models import Services
 
 # Create your views here.
 
@@ -7,10 +10,23 @@ def shop_styles(request):
     """
     A view to show the different styles which the owner has created
     """
+    services = Services.objects.all()
     styles = ShopStyles.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter anything in the search!")
+                return redirect(reverse('products'))
+            
+            queries_styles = Q(style_name__icontains=query) | Q(style_description__icontains=query)
+            styles = styles.filter(queries_styles)
 
     context = {
         'styles': styles,
+        'search_term': query,
     }
 
     return render(request, 'styles/styles.html', context)
