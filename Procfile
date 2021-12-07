9657c27173b736b856d74a83fed90d059fe58c4c
Jacob Molsby <jacobmolsby@hotmail.se>
2021-12-07 07:18:54 +0000

    First Deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn creatorstudio5.wsgi:application
--- creatorstudio5/settings.py
+++ creatorstudio5/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-e#@wg49m5f*ctp9#f932@jpup9e=42%9h0$(t7ttx8n8233vy#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['milestone-5-creator.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -122,12 +122,17 @@ WSGI_APPLICATION = 'creatorstudio5.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 
