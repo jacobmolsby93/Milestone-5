9808ec4541edd2d7872caedc8f0fe4a390522a50
Jacob Molsby <jacobmolsby@hotmail.se>
2021-12-07 07:29:24 +0000

    Removed Secret key from settings

--- creatorstudio5/settings.py
+++ creatorstudio5/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e#@wg49m5f*ctp9#f932@jpup9e=42%9h0$(t7ttx8n8233vy#'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['milestone-5-creator.herokuapp.com', 'localhost']
 
@@ -133,7 +133,6 @@ else:
             'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
-
 
 
 # Password validation
