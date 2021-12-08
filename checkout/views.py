822897c548523b4f4f23de94ccbe201b62c22193
Jacob Molsby <jacobmolsby@hotmail.se>
2021-12-08 04:58:54 +0000

    Fixed order_history and save to profile

--- checkout/views.py
+++ checkout/views.py
@@ -126,10 +126,25 @@ def checkout_success(request, order_number):
     """
     Handle successful checkouts
     """
-
-    url = request.session.get('url')
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
+
+    if request.user.is_authenticated:
+        profile = UserProfile.objects.get(user=request.user)
+        # Attach the user's profile to the order
+        order.user_profile = profile
+        order.save()
+
+        # Save the user's info
+        if save_info:
+            profile_data = {
+                'default_phone_number': order.phone_number,
+                'default_country': order.country,
+                'default_postcode': order.postcode,
+            }
+            user_profile_form = UserProfileForm(profile_data, instance=profile)
+            if user_profile_form.is_valid():
+                user_profile_form.save()
 
     messages.success(request, f'Order successfully processed! \
         Your order number is {order_number}. A confirmation \
@@ -137,11 +152,10 @@ def checkout_success(request, order_number):
 
     if 'bag' in request.session:
         del request.session['bag']
-        
+
     template = 'checkout/checkout_success.html'
     context = {
         'order': order,
-        'url': url,
     }
 
     return render(request, template, context)
--- profiles/views.py
+++ profiles/views.py
@@ -16,9 +16,11 @@ def profile(request):
         form = UserProfileForm(request.POST, instance=profile)
         if form.is_valid():
             form.save()
-            messages.success(request, 'Profile updated Successfully!')
+            messages.success(request, 'Profile updated successfully')
         else:
-            messages.error(request, 'Update failed. Please ensure the form is valid!')
+            messages.error(request,
+                           ('Update failed. Please ensure '
+                            'the form is valid.'))
     else:
         form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
@@ -27,7 +29,7 @@ def profile(request):
     context = {
         'form': form,
         'orders': orders,
-        'on_profile_page': True,
+        'on_profile_page': True
     }
 
     return render(request, template, context)
@@ -35,7 +37,7 @@ def profile(request):
 
 def order_history(request, order_number):
     order = get_object_or_404(Order, order_number=order_number)
-    
+
     messages.info(request, (
         f'This is a past confirmation for order number {order_number}. '
         'A confirmation email was sent on the order date.'
@@ -44,7 +46,7 @@ def order_history(request, order_number):
     template = 'checkout/checkout_success.html'
     context = {
         'order': order,
-        'from_profile': True
+        'from_profile': True,
     }
 
     return render(request, template, context)
