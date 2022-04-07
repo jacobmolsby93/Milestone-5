a86c8429ed70ec28041d2a3428f68fe5378b9b8d
Jacob Molsby <jacobmolsby@hotmail.se>
2022-04-07 08:51:51 +0000

    All forms fixed for responsive layout

--- services/forms.py
+++ services/forms.py
@@ -7,17 +7,54 @@ class RequestForm(forms.ModelForm):
         model = ServiceRequestModel
         fields = ('full_name', 'email', 'phone_number', 'ideas')
         
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholder and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'full_name': 'Full Name',
+            'email': 'Email Address',
+            'phone_number': 'Phone Number',
+            'ideas': 'Idea',
+        }
+        
+        for field in self.fields:            
+            placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+
 
 class ServiceForm(forms.ModelForm):
 
     class Meta:
         model = Services
-        fields = '__all__'
-
+        fields = (
+            'name',
+            'description',
+            'price',
+            'pricing_description',
+            'service_image',
+            'sku')
 
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        Services.objects.all()
+        """
+        Add placeholder and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
 
-        for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'border-black rounded-0'
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'name': 'Service Name',
+            'description': 'Service Description',
+            'price': 'Price',
+            'pricing_description': 'Price Description',
+            'service_image': 'Service Image',
+            'sku': 'SKU - Unique'
+        }
+        
+        for field in self.fields:            
+            placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+
